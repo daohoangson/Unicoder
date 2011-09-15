d8ad4923f43f80c65196667baad1b8c71d1d4d8a
Đào Hoàng Sơn <daohoangson@gmail.com>
2011-09-15 16:45:10 +0700

    Added more comments

--- Unicoder.php
+++ Unicoder.php
@@ -1,6 +1,13 @@
 <?php
 if (!class_exists('Unicoder')): 
+
 class Unicoder {
+	
+	/**
+	 * Removes accents from Unicode string (case sensitive).
+	 * This function supports both precomposed characters and composite characters.
+	 * The string should be a valid utf-8 string (no html stuff or whatever)
+	**/
 	public static function removeAccent($text) {
 		static $map = array(
 			'a' => array('à','á','ạ','ả','ã','â','ầ','ấ','ậ','ẩ','ẫ','ă','ằ','ắ','ặ','ẳ','ẵ'),
@@ -21,14 +28,21 @@ class Unicoder {
 		);
 		$result = $text;
 		foreach ($map as $char => $chars) {
-			$result = str_replace($chars,$char,$result);
+			$result = str_replace($chars, $char, $result);
 		}
 		return $result;
 	}
 	
 	/**
-	 * removes lowered case with accent translated to anscii characters
-	 * safe to use with search functions
+	 * Translates unicode characters to a specialized ansii presentation (case insensitive).
+	 * Please note that this function was designed works with lower case characters, 
+	 * so if your string is not in lower case, make sure to set $inLowerCase to true.
+	 * However, the result will always be in lower case. This function supports both
+	 * precomposed and composite characters.
+	 *
+	 * Example of translated presentation:
+	 * "Hồ Chí Minh" -> "ho^` chi' minh"
+	 * "Cộng hoà xã hội chủ nghĩa Việt Nam" -> "cong^. hoa` xa~ hoi^. chu? nghia~ viet^. nam"
 	**/
 	public static function asciiAccent($text, $inLowerCase = false) {
 		static $map = array(
@@ -52,16 +66,23 @@ class Unicoder {
 		static $marks = array('`',"'",'.','?','~');
 
 		// convert characters
-		$text = str_replace($keys,$map,($inLowerCase?$text:self::strtolower($text)));
+		$text = str_replace($keys, $map, ($inLowerCase ? $text : self::strtolower($text)));
 		
 		$words = explode(' ',$text);
-		for ($i = 0; $i < count($words); $i++) {
-			// count($words) penalty is low enough to just ignore it
+		$count = count($words);
+		for ($i = 0; $i < $count; $i++) {
 			foreach ($marks as $mark) {
-				$pos = strpos($words[$i],$mark);
+				$pos = strpos($words[$i], $mark);
+				/*
+				Note: It's safe to use strpos here because the whole string
+				is now ascii characters. We don't need to hurt performance using
+				the heavier version functions like mb_strpos, mb_strlen or mb_substr
+				*/
 				if ($pos !== false AND $pos < strlen($words[$i]) - 1) {
-					// move it
-					$words[$i] = substr($words[$i],0,$pos) . substr($words[$i],$pos + 1) . $mark;
+					// this word have a mark somewhere, move it so it will be the last character
+					$words[$i] = substr($words[$i], 0, $pos) // the portion before the mark
+						. substr($words[$i], $pos + 1) // the portion after the mark
+						. $mark; // the mark itself
 				}
 			}
 		}
@@ -69,9 +90,15 @@ class Unicoder {
 		return implode(' ',$words);
 	}
 	
+	/**
+	 * Converts case of characters. 
+	 * This function supports two way conversion. To make the string in lower case,
+	 * leave the $reverse as false, otherwise, make it true. In theory, this function
+	 * should work with both precomposed and composite characters (but I didn't test, sorry).
+	**/
 	public static function strtolower($text, $reverse = false) {
 		static $map = array(
-			// lower case (67)
+			// lower case (67 chars)
 			'à','á','ạ','ả','ã','â','ầ','ấ','ậ','ẩ','ẫ','ă','ằ','ắ','ặ','ẳ','ẵ',
 			'è','é','ẹ','ẻ','ẽ','ê','ề','ế','ệ','ể','ễ',
 			'ì','í','ị','ỉ','ĩ',
@@ -79,7 +106,7 @@ class Unicoder {
 			'ù','ú','ụ','ủ','ũ','ư','ừ','ứ','ự','ử','ữ',
 			'ỳ','ý','ỵ','ỷ','ỹ',
 			'đ',
-			// upper case (67)
+			// upper case (67 chars)
 			'À','Á','Ạ','Ả','Ã','Â','Ầ','Ấ','Ậ','Ẩ','Ẫ','Ă','Ằ','Ắ','Ặ','Ẳ','Ẵ',
 			'È','É','Ẹ','Ẻ','Ẽ','Ê','Ề','Ế','Ệ','Ể','Ễ',
 			'Ì','Í','Ị','Ỉ','Ĩ',
@@ -93,6 +120,8 @@ class Unicoder {
 		static $regex = null;
 		
 		if (empty($lower)) {
+			// build the conversion array first time this function runs
+			// simply merge ascii characters with unicode characters
 			$lower = array_slice($map,0,67);
 			$upper = array_slice($map,67);
 			for ($i = 65; $i <= 90; $i++) {
@@ -103,31 +132,45 @@ class Unicoder {
 		}
 		
 		if (empty($reverse)) {
-			$text = str_replace($upper,$lower,$text);
+			// strtolower
+			$text = str_replace($upper, $lower, $text);
 		} else {
-			$text = str_replace($lower,$upper,$text);
+			// strtoupper
+			$text = str_replace($lower, $upper, $text);
 		}
 		
 		return $text;
 	}
 	
+	/**
+	 * Converts case of characters from lower case to upper case. 
+	 * In theory, this function should work with both precomposed and 
+	 * composite characters (but I didn't test, sorry).
+	**/
 	public static function strtoupper($text) {
 		return self::strtolower($text,true);
 	}
 	
+	/**
+	 * Returns a string with the first character of each word in str capitalized,
+	 * if that character is alphabetic.
+	 * The definition of a word is any string of characters that is immediately after a space (ascii #32).
+	 * This function works with both precomposed and composite characters.
+	**/
 	public static function ucwords($text) {
-		$words = explode(' ',self::strtolower($text));
-		for ($i=0; $i < count($words); $i++) { 
-			for ($j=0; $j < strlen($words[$i]); $j++) { 
-				$sample = substr($words[$i],0,$j + 1);
-				$changed = self::strtoupper($sample);
-				if ($changed != $sample) {
-					$words[$i] = $changed . substr($words[$i],$j + 1);
-					break;
-				}
+		$words = explode(' ', self::strtolower($text));
+		$count = count($words);
+		
+		for ($i = 0; $i < $count; $i++) {
+			$sample = mb_substr($words[$i], 0, 1);
+			$changed = self::strtoupper($sample);
+			if ($changed != $sample) {
+				$words[$i] = $changed . mb_substr($words[$i], 1);
 			}
 		}
+		
 		return implode(' ',$words);
 	}
 }
+
 endif;
